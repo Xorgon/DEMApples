f0184dec77569fe3b0487e97444443df13125a05
Xorgon <norman.reggox@gmail.com>
2017-11-15 04:03:35 +0000

    Added warning for future debugging.

--- dem_sim/util/vector_utils.py
+++ dem_sim/util/vector_utils.py
@@ -3,4 +3,9 @@ import numpy as np
 
 def normalize(vector):
     vector = np.array(vector)
-    return vector / np.linalg.norm(vector)
+    if np.linalg.norm(vector) == 0:
+        print("Warning, encountered 0 length vector when attempting to normalize.")
+        # TODO: Fix this!!
+        return vector
+    else:
+        return vector / np.linalg.norm(vector)
