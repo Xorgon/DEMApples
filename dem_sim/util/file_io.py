e1acba6328402b4f10538ff17fb8e000e0af892c
Xorgon <norman.reggox@gmail.com>
2017-11-15 04:01:28 +0000

    Added ability to set the number of frames to log per second.

--- dem_sim/util/file_io.py
+++ dem_sim/util/file_io.py
@@ -50,7 +50,7 @@ def particles_to_binary(particles, filename_root, path="", ignore_warnings=False
                                              num_utils.float_to_bin(p.get_speed_at_index(i))))
 
 
-def particles_to_paraview(particles, filename_root, path="", ignore_warnings=False):
+def particles_to_paraview(particles, filename_root, path="", ignore_warnings=False, fps=60):
     if os.path.exists(path) and not ignore_warnings:
         ans = input(path + " already exists. Remove and continue? [y/N]\n")
         cont = True
@@ -81,11 +81,22 @@ def particles_to_paraview(particles, filename_root, path="", ignore_warnings=Fal
         os.mkdir(path)
 
     times = particles[0].times
+    log_step = 1 / fps
+    last_log = None
+    print("At {0} frames per second, logging every {1} seconds.".format(fps, log_step))
     for i in range(len(times)):
-        # TODO: Skip some log files.
-        millis = int(times[i] * 1000)
-        file = open(path + filename_root + "_" + str(millis) + ".txt", 'w')
-        for p in particles:
-            file.write("{0:.5f},{1:.5f},{2:.5f},{3:.5f}\n".format(p.pos_history[i][0], p.pos_history[i][1],
-                                                                  p.pos_history[i][2],
-                                                                  p.get_speed_at_index(i)))
+        log = False
+        if last_log is None:
+            last_log = times[i]
+            log = True
+        elif times[i] - last_log >= log_step:
+            last_log = times[i]
+            log = True
+
+        if log:
+            millis = int(times[i] * 1000)
+            file = open(path + filename_root + "_" + str(millis) + ".txt", 'w')
+            for p in particles:
+                file.write("{0:.5f},{1:.5f},{2:.5f},{3:.5f}\n".format(p.pos_history[i][0], p.pos_history[i][1],
+                                                                      p.pos_history[i][2],
+                                                                      p.get_speed_at_index(i)))
